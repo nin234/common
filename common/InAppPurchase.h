2e3df096665e68a7832197f11aa3208f3aac8114
Ninan Thomas <nin234@yahoo.com>
2016-02-26 22:52:58 -0500

    common code refactoring

---
+++ common/InAppPurchase.h
@@ -0,0 +1,31 @@
+//
+//  InAppPurchase.h
+//  Shopper
+//
+//  Created by Ninan Thomas on 2/28/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <StoreKit/StoreKit.h>
+#import <UIKit/UIKit.h>
+
+@protocol InAppPurchaseDelegate <NSObject>
+
+@required
+-(void) setPurchsd;
+
+@end
+
+@interface InAppPurchase : NSObject<UIAlertViewDelegate, SKProductsRequestDelegate, SKPaymentTransactionObserver>
+{
+    SKProduct *product;
+    SKProductsRequest *productsRequest;
+    bool bIgnoreAlertVwClck;
+    bool bPurchase;
+}
+
+-(void) start:(bool) purchase;
+@property(nonatomic, weak) id<InAppPurchaseDelegate> delegate;
+
+@end
---
+++ common/textdefs.h
@@ -0,0 +1,41 @@
+//
+//  textdefs.h
+//  Shopper
+//
+//  Created by Ninan Thomas on 2/26/15.
+//
+//
+
+#ifndef Shopper_textdefs_h
+#define Shopper_textdefs_h
+
+
+#define HOUSE_NAME 0
+#define HOUSE_PRICE 1
+#define HOUSE_AREA 2
+#define HOUSE_YEAR 3
+#define HOUSE_BEDS 4
+#define HOUSE_BATHS 5
+#define HOUSE_STREET 6
+#define HOUSE_CITY 7
+#define HOUSE_STATE 8
+#define HOUSE_COUNTRY 9
+#define HOUSE_ZIP 10
+
+#define CAR_NAME 0
+#define CAR_MODEL 1
+#define CAR_COLOR 2
+#define CAR_MAKE 3
+#define CAR_YEAR 4
+#define CAR_PRICE 5
+#define CAR_MILES 6
+#define CAR_STREET 7
+#define CAR_CITY 8
+#define CAR_STATE 9
+#define CAR_COUNTRY 10
+#define CAR_ZIP 11
+
+
+
+
+#endif
