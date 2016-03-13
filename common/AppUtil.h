c6982bf2bf3c99b53d2bfe7f2cc02078097647a7
Rekha Thomas <rekha_susan@hotmail.com>
2016-03-12 20:15:31 -0500

    Refactoring of AppDelegate start

---
+++ common/AppUtil.h
@@ -0,0 +1,15 @@
+//
+//  AppUtil.h
+//  common
+//
+//  Created by Rekha Thomas on 3/12/16.
+//  Copyright © 2016 Sinacama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppUtil : NSObject
+
+- (NSString *) getAlbumDir: (NSString *) album_name;
+
+@end
---
+++ common/AppUtil.m
@@ -0,0 +1,24 @@
+//
+//  AppUtil.m
+//  common
+//
+//  Created by Rekha Thomas on 3/12/16.
+//  Copyright © 2016 Sinacama. All rights reserved.
+//
+
+#import "AppUtil.h"
+
+@implementation AppUtil
+
+- (NSString *) getAlbumDir: (NSString *) album_name
+{
+    NSString *pHdir = NSHomeDirectory();
+    NSString *pAlbums = @"/Documents/albums";
+    NSString *pAlbumsDir = [pHdir stringByAppendingString:pAlbums];
+    pAlbumsDir = [pAlbumsDir stringByAppendingString:@"/"];
+    NSString *pNewAlbum = [pAlbumsDir stringByAppendingString:album_name];
+    NSURL *url = [NSURL fileURLWithPath:pNewAlbum isDirectory:YES];
+    return [url absoluteString];
+}
+
+@end
