ab8d9e0c107f7962299d96b48196e06bba93e343
Ninan Thomas <ninanthomas@ninans-mbp.home>
2017-03-11 22:24:00 -0500

    moved from EasyGrocList

---
+++ common/List.h
@@ -0,0 +1,21 @@
+//
+//  List.h
+//  EasyGrocList
+//
+//  Created by Ninan Thomas on 5/19/13.
+//  Copyright (c) 2013 Ninan Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface List : NSManagedObject
+
+@property long long rowno;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * item;
+@property (nonatomic, retain) NSDate   *date;
+@property BOOL hidden;
+
+
+@end
